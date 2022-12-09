3220d560bd695989d14c3763c76b5eaf2b5f9a03
Chris Ferenci <chrisferenci@berkeley.edu>
2022-12-09 13:09:16 -0800

    move loader to browse jobs and add images to landing page

--- project-code/src/pages/Home.js
+++ project-code/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { createContext, useEffect, useState } from 'react';
 import Header from '../components/Header/Header';
 import Sidebar from '../components/Sidebar/Sidebar';
 import JobBoard from '../components/JobBoard/JobBoard';
+import Loader from '../components/Loader/Loader';
 
 
 import './home.css';
@@ -13,14 +14,26 @@ export const CountryContext = createContext();
 
 function Main() {
 
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 1300);
+    }, []);
+
         
     const [activeCountry, setActiveCountry] = useState('United States');
     const [activeExperience, setActiveExperience] = useState('')
     const [jobs, setJobs] = useState('');
     const [searchQuery, setSearchQuery] = useState('');
     const [favorites, setFavorites] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+ 
 
     function hitAPI () {
+
+        
 
         let jobs_data = [];
         fetch('https://api.reliefweb.int/v1/jobs?appname=apidoc&profile=list&preset=latest&&fields[include][]=experience.name&filter[operator]=AND&filter[conditions][0][field]=country&filter[conditions][0][value]='+activeCountry+'&filter[operator]=AND&filter[conditions][1][field]=experience.name&filter[conditions][1][value]='+activeExperience).then((response) => response.json()).then((json_item) => {
@@ -49,15 +62,27 @@ function Main() {
         hitAPI();
     }, [activeCountry, activeExperience]);
 
+    
+
+
+
 
     return(
+        <>
+        { loading ? ( <Loader /> ) :
+        (
         <div id='grid-container'>
+            
             <CountryContext.Provider value={[activeCountry, setActiveCountry, activeExperience, setActiveExperience, jobs, setJobs, searchQuery, setSearchQuery, favorites, setFavorites]}>
                 <Header />
                 <Sidebar />
                 <JobBoard />
             </CountryContext.Provider>
+        
         </div>
+        )}
+        </>
+        
     )
 }
 
